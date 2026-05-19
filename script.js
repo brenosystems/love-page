00b4a8b6aeda1ffed8150633d97e026605bf30ac
Breno Santos Souza da Silva <devbrenosouza@gmail.com>
2026-05-18 22:03:02 -0300

    atualização
    modifiquei tudo, deixei tudo mais bonito, coloquei imagens, videos, animações e musicas

--- script.js
+++ script.js
@@ -1,36 +1,15 @@
-const cards = document.querySelectorAll('.card');
+const flipCards = document.querySelectorAll('.flip-card');
 
-window.addEventListener('scroll', () => {
-  cards.forEach(card => {
-    const cardTop = card.getBoundingClientRect().top;
-
-    if(cardTop < window.innerHeight - 100) {
-      card.style.opacity = '1';
-      card.style.transform = 'translateY(0)';
-    }
+flipCards.forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('active');
   });
 });
 
-cards.forEach(card => {
-  card.style.opacity = '0';
-  card.style.transform = 'translateY(50px)';
-  card.style.transition = '0.8s';
+const music = document.getElementById('bg-music');
+const startBtn = document.getElementById('start-btn');
+
+startBtn.addEventListener('click', () => {
+  music.volume = 0.4;
+  music.play();
 });
-
-const title = document.querySelector('.hero h1');
-
-setInterval(() => {
-  title.style.textShadow = `
-    0 0 20px #ffd700,
-    0 0 40px #ffcc00,
-    0 0 80px rgba(255,215,0,0.9)
-  `;
-
-  setTimeout(() => {
-    title.style.textShadow = `
-      0 0 10px #ffd700,
-      0 0 20px #ffcc00
-    `;
-  }, 1500);
-
-}, 3000);
